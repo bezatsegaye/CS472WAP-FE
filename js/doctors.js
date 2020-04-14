138f631ce3e8cd31f237cbb11936fc4401c9ba38
Kanharith SOK <kanharithsok@gmail.com>
2020-04-14 01:54:01 -0500

    Refactoring

--- js/doctors.js
+++ js/doctors.js
@@ -1,28 +1,34 @@
 "use strict";
 
 window.onload = () => {
-    getDoctors()
+
+    getDoctors();
 
     function getDoctors() {
         fetch("../json/doctors.json")
             .then(response => response.json())
             .then((doctors) => {
                 doctors.forEach((item, index) => {
-                    const fullName = [item.firstName, item.middleName, item.lastName]
-                        .filter(item => item !== null)
-                        .join(" ")
-                    const row = `
-                    <tr>
-                        <th scope="row">${index + 1}</th>
-                        <td>${item.id}</td>
-                        <td>${fullName}</td>
-                        <td>${item.phoneNumber}</td>
-                        <td>${item.address}</td>
-                    </tr>
-                    `;
+                    const row = buildRow(item, index);
                     $(table).find('tbody').append(row);
                 })
             })
             .catch(error => alert(error));
+    }
+
+    function buildRow(doctor, index) {
+        const fullName = [doctor.firstName, doctor.middleName, doctor.lastName]
+            .filter(item => item !== null)
+            .join(" ")
+        const row = `
+            <tr>
+                <th scope="row">${index + 1}</th>
+                <td>${doctor.id}</td>
+                <td>${fullName}</td>
+                <td>${doctor.phoneNumber}</td>
+                <td>${doctor.address}</td>
+            </tr>
+            `;
+        return row;
     }
 }
